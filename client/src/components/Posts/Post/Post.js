8b90edc8afec84448eccef8a601dd3573bd968b8
mujeeb olagunju <maoltech27@gmail.com>
2022-10-13 05:28:55 +0100

    working

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,12 +1,46 @@
 import React from  'react';
 import useStyles from  './style';
+import { Card, CardActions, CardContent, CardMedia, Button, Typography } from '@material-ui/core';
+import ThumbUpAltIcon from '@material-ui/icons-material/ThumbUpAlt';
+import DeleteIcon from '@material-ui/icons-material/Delete';
+import MoreHorizIcon from '@material-ui/icons-material/MoreHoriz';
+import moment from 'moment';
 
-const Post = () =>{
+const Post = ({post}) =>{
 
     const classes = useStyles();
     
     return(
-        <h1 className={classes.header}>Post</h1>
+        <Card className={classes.card}>
+            <CardMedia className={classes.media} image={post.selectedFile} title={post.title} />
+            <div className={classes.overlay}>
+                <Typography variant='h6'>{post.creator}</Typography>
+                <Typography variant='body2'>{moment(post.createAt).fromNow()}</Typography>
+            </div>
+            <div className={classes.overlay2}>
+                <Button style={{color: 'white'}} size='small' onClick={()=>{}}>
+                    <MoreHorizIcon fontSize='default' />
+                </Button>
+            </div>
+            <div className={classes.details}>
+                <Typography variant='body2' color='textSecondary'>{post.tags.map((tag)=>)}</Typography>
+            </div>
+            <CardContent>
+                <Typography className={classes.title} variant='h5' gutterButton>{post.message}</Typography>
+            </CardContent>
+            <CardActions className={classes.cardActions}>
+                <Button size='small' color='primary' onClick={()=>{}}>
+                    <ThumbUpAltIcon fontSize='small'/>
+                    Like
+                    {post.likeCount}
+                </Button>
+                <Button size='small' color='primary' onClick={()=>{}}>
+                    <DeleteIcon fontSize='small'/>
+                    Delete
+                </Button>
+            </CardActions>
+
+        </Card>
     );
 }
 
